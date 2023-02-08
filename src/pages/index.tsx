685b5bd3eaab95f504b7f226d7b2a6e26c7c6cf1
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-02-08 11:21:24 -0300

    feat: adicionado css global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import styles from '../styles/styles.module.scss'
-export default function Home() {
-  return (
-      <main>
-        <div className={ styles.container }>
-          <h1>OI sou o Tarefator</h1>
-        </div>
-      </main>
+import Head from "next/head";
+import styles from "../styles/styles.module.scss";
 
-  )
+export default function Home() {
+	return (
+		<>
+      <Head>
+        <title>Tarefator App</title>
+      </Head>
+			<main>
+				<div className={styles.container}>
+					<h1>OI sou o Tarefator</h1>
+				</div>
+			</main>
+		</>
+	);
 }
